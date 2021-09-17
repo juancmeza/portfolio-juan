c89ac8a25603cf606aec498aa23c0787761e840b
Juan Carlos Meza <juancmezat@gmail.com>
2021-09-17 01:28:24 -0700

    Social Media Buttons Styled

--- src/components/About.js
+++ src/components/About.js
@@ -4,6 +4,7 @@ import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import IconButton from '@material-ui/core/IconButton';
+import SocialMedia from './SocialMedia'
 
 
 function About() {
@@ -19,15 +20,13 @@ function About() {
 
   return (
     <div id="AboutContainer">
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
+        <div style={{height: '10vh'}}></div>
         <div id="About">
             <TypeWriter typing={0.5} onTypingEnd={() => setDisplayAboutParagraph(true)}>
                 Hello
                 <div style={{color: '#3b79ac'}}> World!!!</div>
             </TypeWriter>
+            <div style={{height: '5vh'}}></div>
             {displayAboutParagraph ? 
                 <div id="AboutParagraph">
                     <div>
@@ -38,15 +37,7 @@ function About() {
                         based in the San Francisco Bay Area currently working at AAK-TELESCIENCE, INC.
                     </div>
                     <div className="SocialMedia">
-                        <IconButton>
-                            <LinkedInIcon fontSize="large"></LinkedInIcon>
-                        </IconButton>
-                        <IconButton>
-                            <GitHubIcon fontSize="large"></GitHubIcon>
-                        </IconButton>
-                        <IconButton>
-                            <InstagramIcon fontSize="large"></InstagramIcon>
-                        </IconButton>
+                        <SocialMedia></SocialMedia>
                     </div>
                 </div>
                 :
---
+++ src/components/SocialMedia.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import IconButton from '@material-ui/core/IconButton';
+
+
+const useStyles = makeStyles((theme) => ({
+
+    action: {
+      backgroundColor: 'rgba(0, 0, 0, .5)',
+      color: 'rgb(212, 222, 230, 0.9)',
+      marginBottom: -100,
+      marginTop: -80,
+      marginRight: 10,
+      marginLeft: 10,
+      boxShadow: '0 1px 4px 0 rgba(0,0,0,0.12)',
+      '&:hover': {
+        backgroundColor: '#fff',
+        color: '#C0C0C0',
+      },
+    }
+  }));  
+
+function About() {
+
+  const styles = useStyles();
+
+  return (
+    <div>
+        <IconButton className={styles.action}>
+            <LinkedInIcon fontSize="large"></LinkedInIcon>
+        </IconButton>
+        <IconButton className={styles.action}>
+            <GitHubIcon fontSize="large"></GitHubIcon>
+        </IconButton>
+        <IconButton className={styles.action}>
+            <InstagramIcon fontSize="large"></InstagramIcon>
+        </IconButton>
+    </div>
+  );
+}
+
+export default About;

969697422ee8a146b99b6ff7a7c81fa946370e6e
Juan Carlos Meza <juancmezat@gmail.com>
2021-09-25 02:48:57 -0700

    Favicon + Contact. Missing mailer, footer, and animations.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import ContactIcons from './ContactIcons.js'
 
 function Contact() {
   return (
     <div id="Contact">
         <div style={{height: '5vh'}}></div>
         <h2 className="ComponentTitle FontGradient">Contact</h2>
+        <div id="ContactContainer">
+            <div id="ProfilePicture"></div>
+            <div id="ContactText">
+              Do you think I would be a good fit for your team?
+            </div>
+            <div id="ContactText">
+              Let's connect on LinkedIn or shoot me an email.
+            </div>
+            <ContactIcons></ContactIcons>
+        </div>
     </div>
   );
 }
--- src/components/ContactIcons.js
+++ src/components/ContactIcons.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import InstagramIcon from '@mui/icons-material/Instagram';
+import EmailIcon from '@mui/icons-material/Email';
 import IconButton from '@material-ui/core/IconButton';
 
 
@@ -10,42 +9,35 @@ const useStyles = makeStyles((theme) => ({
 
     action: {
       backgroundColor: 'rgba(0, 0, 0, .5)',
-      color: 'rgb(212, 222, 230, 0.9)',
-      marginBottom: -100,
-      marginTop: -80,
+      color: '#a0d0ec',
       marginRight: 10,
       marginLeft: 10,
       boxShadow: '0 1px 4px 0 rgba(0,0,0,0.12)',
       '&:hover': {
-        backgroundColor: '#fff',
-        color: '#C0C0C0',
+        backgroundColor: '#304369',
+        // color: '#376ec0',
       },
     }
   }));  
 
-function SocialMedia() {
+function ContactIcons() {
 
   const styles = useStyles();
 
   return (
-    <div>
+    <div className="ContactIcons">
         <a href="https://www.linkedin.com/in/juan-meza-36065974/">
             <IconButton className={styles.action}>
                 <LinkedInIcon fontSize="large"></LinkedInIcon>
             </IconButton>
         </a>
-        <a href="https://github.com/juancmeza">
-            <IconButton className={styles.action}>
-                <GitHubIcon fontSize="large"></GitHubIcon>
-            </IconButton>
-        </a>
         <a href="https://www.instagram.com/juanforthebookz/">
             <IconButton className={styles.action}>
-                <InstagramIcon fontSize="large"></InstagramIcon>
+                <EmailIcon fontSize="large"></EmailIcon>
             </IconButton>
         </a>
     </div>
   );
 }
 
-export default SocialMedia;
+export default ContactIcons;

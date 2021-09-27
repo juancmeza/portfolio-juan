26678dcc93ab0d578a7fa59dc9062162a1c6ebb6
Juan Carlos Meza <juancmezat@gmail.com>
2021-09-27 10:27:56 -0700

    Different approach to transparent fonts

--- src/components/About.js
+++ src/components/About.js
@@ -22,40 +22,40 @@ function About() {
                         found a newly sparked interest in the creative possibilities of the cocktail world.
                     </li>
                     <li>
-                        7 years later, I knew it was time to seek new challenges, and after some exploration,
+                        7 years later, I knew it was time to seek new challenges and after some exploration,
                         I decided to enroll in Flatiron's Software Engineering bootcamp, where I learned
                         RESTful APIs, agile development, and many of the technical skills below.
                     </li>
                 </ul>
             </div>
             <div id="About2">
-                <div className="TechnicalSkills FontGradient" >
-                    <h3>Languages</h3>
-                    <p>Javascript</p>
-                    <p>Ruby</p>
-                    <p>Python</p>
-                    <p>SQL</p>
-                    <p>PHP</p>
+                <div className="TechnicalSkills">
+                    <h3 className="FontGradient">Languages</h3>
+                    <li>Javascript</li>
+                    <li>Ruby</li>
+                    <li>Python</li>
+                    <li>SQL</li>
+                    <li>PHP</li>
                     {/* <li>R</li> */}
-                    <p>HTML5</p>
-                    <p>CSS3</p>
+                    <li>HTML5</li>
+                    <li>CSS3</li>
                 </div>
-                <div className="TechnicalSkills FontGradient">
-                    <h3>Frameworks</h3>
-                    <p>React</p>
-                    <p>Ruby on Rails</p>
-                    <p>Bootstrap</p>
-                    <p>Material UI</p>
-                    <p>Semantic UI</p>
+                <div className="TechnicalSkills">
+                    <h3 className="FontGradient">Frameworks</h3>
+                    <li>React</li>
+                    <li>Ruby on Rails</li>
+                    <li>Bootstrap</li>
+                    <li>Material UI</li>
+                    <li>Semantic UI</li>
                 </div>
-                <div className="TechnicalSkills FontGradient">
-                    <h3>Other</h3>
-                    <p>RSpec</p>
-                    <p>Jest</p>
-                    <p>Heroku</p>
-                    <p>Firebase</p>
-                    <p>Netlify</p>
-                    <p>Postman</p>
+                <div className="TechnicalSkills">
+                    <h3 className="FontGradient">Other</h3>
+                    <li>RSpec</li>
+                    <li>Jest</li>
+                    <li>Heroku</li>
+                    <li>Firebase</li>
+                    <li>Netlify</li>
+                    <li>Postman</li>
                 </div>
             </div>
         </div>

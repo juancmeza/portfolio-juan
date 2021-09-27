ad52fe2412d8d1064ecfd2b25733427ab1c1a60d
Juan Carlos Meza <juancmezat@gmail.com>
2021-09-26 17:51:53 -0700

    First version ready!

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
   return (
     <div className="App">
       <h1 id="Title" className="FontGradient">Juan Carlos Meza</h1>
+      <div className="App-logo"></div>
       <Nav></Nav>
       <Home></Home>
       <About></About>
